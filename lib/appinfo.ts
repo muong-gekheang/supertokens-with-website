caf6a20a314bf9fc2a20d5e112a3d40eec0edafc
muong-gekheang <muonggekheang05@gmail.com>
2026-06-09 13:58:25 +0700

    fix app info

--- lib/appinfo.ts
+++ lib/appinfo.ts
@@ -1,6 +1,6 @@
 export const appInfo = {
   appName: "My App",
-  apiDomain: "https://dev2.power-hub.app", // 👈 Your NestJS backend port
+  apiDomain: "https://beta.power-hub.app", // 👈 Your NestJS backend port
   websiteDomain: "https://supertokens-with-website.vercel.app", // 👈 Your Next.js port
   apiBasePath: "/auth",
   websiteBasePath: "/auth",
